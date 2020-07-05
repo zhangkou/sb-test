0dd68efe02b3fe39cfb205fca7e8c7d293fc3d6d
Zak.zhang <zak.zhang@qq.com>
2020-07-05 14:01:02 +0800

    ...

--- src/main/java/com/example/sbtest/SbtestApplication.java
+++ src/main/java/com/example/sbtest/SbtestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@RestController("/sbtest")
 public class SbtestApplication {
 	
 	@RequestMapping("/")
